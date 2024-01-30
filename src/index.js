ee147730afdf2ed736e3eaa29f8736cad4ef9702
kldieselberg <kldieselberg@gmail.com>
2024-01-30 14:19:03 -0600

    search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#current-temp");
+  let realtimeTemp = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-current-city");
+  cityElement.innerHTML = response.data.city;
+  temperature.innerHTML = Math.round(realtimeTemp);
+}
+
+function searchCity(city) {
+  let apiKey = "f6fb43beoc84b39t6ab37d400ab015a0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function citySearchResult(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#citySearched");
-  let cityElement = document.querySelector("#weather-current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySearchResult);
+
+searchCity("Bangkok");
